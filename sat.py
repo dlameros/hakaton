94d6db37f2f519f1428fe8da969994e5456989db
dlameros <Merkov03@list.ru>
2023-10-26 02:36:38 +0300

    3
    обновляющиеся спутники, список спутников

--- sat.py
+++ sat.py
@@ -1,35 +1,55 @@
 from sgp4.earth_gravity import wgs72
 from sgp4.io import twoline2rv
-from sgp4.propagation import sgp4
-from math import degrees
+from math import radians, cos, sin, degrees
 import datetime
-from math import radians, cos, sin
-line1 = '1 06235U 72082A   23297.48450924 -.00000006  00000-0  25309-3 0  9993'
-line2 = '2 06235 102.0042 288.8635 0003770 209.3723 305.2886 12.53169402333857'
+from geopy.point import Point
+from geopy.distance import EARTH_RADIUS
+import math
+import time
+# Функция для вычисления расстояния между двумя точками на Земле
+def calculate_distance(lat1, lon1, lat2, lon2):
+    lat2 = radians(lat2)
+    lon2 = radians(lon2)
 
-satellite = twoline2rv(line1, line2, wgs72)
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
 
-# Создаем объект даты и времени
-year, month, day, hour, minute, second = 2023, 10, 26, 0, 0, 0
-date = datetime.datetime(year, month, day, hour, minute, second)
+    # Расчет гаверсинуса
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon / 2) ** 2
 
-# Вычисляем позицию спутника в заданный момент времени
-position, velocity = satellite.propagate(date.year, date.month, date.day, date.hour, date.minute, date.second)
+    # Расчет центрального угла
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+
+    # Расчет расстояния на поверхности земли
+    r = 6371  # Радиус Земли в километрах
+    dist = r * c
+
+    return dist
+
+# Чтение данных из файла и создание списка спутников
+satellites = []
+with open("satoline.txt", "r") as file:
+    lines = file.readlines()
+    for i in range(0, len(lines), 3):
+        line1 = lines[i+1].strip()
+        line2 = lines[i+2].strip()
+        satellite = twoline2rv(line1, line2, wgs72)
+        satellites.append((lines[i], satellite))
 
 # Координаты точки на Земле
-lat, lon = 55.75, 37.62  # Москва
+target_lat, target_lon = 55.75, 37.62  # Пример координат Москвы
 
-# Меняем углы в радианы
-lat = radians(lat)
-lon = radians(lon)
+# Вычисление расстояния каждого спутника до точки на Земле
+while True:
+    for name, satellite in satellites:
+        date = datetime.datetime.now()
+        position, _ = satellite.propagate(date.year, date.month, date.day, date.hour, date.minute, date.second)
 
-# Радиус Земли в километрах
-radius = 6371
-
-# Вычисляем расстояние
-dx = cos(position[0]) * cos(position[1]) - cos(lat) * cos(lon)
-dy = cos(position[0]) * sin(position[1]) - cos(lat) * sin(lon)
-dz = sin(position[0]) - sin(lat)
-distance = radius * (dx**2 + dy**2 + dz**2)**0.5
-
-print(f"Расстояние от спутника до точки на Земле: {round(distance, 2)} км")
+        x, y, z = position[0], position[1], position[2] 
+        distance = (x ** 2 + y ** 2 + z ** 2) ** 0.5
+        sat_lat = Point(latitude=y/EARTH_RADIUS, longitude=x/EARTH_RADIUS, altitude=z/EARTH_RADIUS).latitude
+        sat_lon = Point(latitude=y/EARTH_RADIUS, longitude=x/EARTH_RADIUS, altitude=z/EARTH_RADIUS).longitude
+        
+        distance = calculate_distance(sat_lat, sat_lon, target_lat, target_lon)
+        print(f"Расстояние до спутника {name}: {distance} км")
+    time.sleep(1)
